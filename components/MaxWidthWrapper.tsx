cb11410aa7e94b0f95124fe3607f47059458d873
anirudh.boxfy <anirudh.boxfy@gmail.com>
2024-01-27 13:56:52 +0530

    First commit

---
+++ components/MaxWidthWrapper.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from "react";
+
+function MaxWidthWrapper({
+	className,
+	children,
+}: {
+	className?: string;
+	children: ReactNode;
+}) {
+	return <div>MaxWidthWrapper</div>;
+}
+
+export default MaxWidthWrapper;
